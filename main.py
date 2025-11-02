e50f43a2362d4466c03efd8d105c196997b14ca2
svetlana-kuprina <kupsveta@rambler.ru>
2025-11-02 18:40:59 +0500

    Выполнение задания 12.2

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
-#number = str(input("Введите название и номер счета или карты: "))
-#str_date = str(input("Введите дату: "))
+number = str(input("Введите название и номер счета или карты: "))
+str_date = str(input("Введите дату: "))
 
-#print(mask_account_card(number))
-#print(get_date(str_date))
+print(mask_account_card(number))
+print(get_date(str_date))
 
-list_test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-print(filter_by_state(list_test))
-
-list_test1 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-print(sort_by_date(list_test1,sort = False))
+# list_test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#
+# print(filter_by_state(list_test))
+#
+# list_test1 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#
+# print(sort_by_date(list_test1,sort = False))
