0dd2e75e69da92236467ed3ea0440e146ac32edf
svetlana-kuprina <kupsveta@rambler.ru>
2025-11-04 23:10:36 +0500

    ДЗ 13.1 final

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -1,20 +1,22 @@
 import csv
+from typing import Hashable, Any
+
 import pandas as pd
 
 
 def read_csv(direct: str) -> list[dict[str, str]]:
-    """ Функция реализует считывание финансовых операций из CSV-файла"""
+    """Функция реализует считывание финансовых операций из CSV-файла"""
     try:
 
         with open(direct, encoding="utf-8") as f:
-            reader = csv.DictReader(f, delimiter=';')
+            reader = csv.DictReader(f, delimiter=";")
             return list(reader)
 
     except FileNotFoundError:
         return []
 
 
-def read_excel(direct: str) -> list[dict[str, str]]:
+def read_excel(direct: str) -> list[dict[Hashable, Any]]:
     """Функция реализует считывание финансовых операций из XLSX-файла"""
     try:
 
--- tests/test_read_csv_excel.py
+++ tests/test_read_csv_excel.py
@@ -1,4 +1,3 @@
-import csv
 from unittest.mock import patch, mock_open
 
 import pandas as pd
@@ -7,7 +6,9 @@ from src.read_csv_excel import read_csv, read_excel
 
 
 @patch("builtins.open", new_callable=mock_open,
-       read_data='id;state;date;amount;currency_name;currency_code;from;to;description\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации')
+       read_data='id;state;date;amount;currency_name;currency_code;from;to;description\n650703;EXECUTED;'
+                 '2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;'
+                 'Перевод организации')
 def test_read_csv(mock_open):
     """Тест функции read_csv. Используется Mock и patch"""
 
@@ -21,10 +22,12 @@ def test_read_csv(mock_open):
     result = read_csv('test.csv')
     assert result == expected_result
 
+
 def test_read_csv_error():
     """Тест функции read_csv. Ошибка файл не найден"""
 
     assert read_csv(" ") == []
+
 
 def test_read_excel_error():
     """Тест функции read_excel. Ошибка файл не найден"""
@@ -37,18 +40,18 @@ def test_read_excel(mock_df):
     """Тест функции read_excel. Используется Mock и patch"""
 
     read_data = pd.DataFrame({
-                    'id': ['650703'], 'state': ['EXECUTED'], 'date': ['2023-09-05T11:30:32Z'], 'amount': ['16210'],
-                    'currency_name': ['Sol'], 'currency_code': ['PEN'], 'from': ['Счет 58803664561298323391'],
-                    'to': ['Счет 39745660563456619397'], 'description': ['Перевод организации']
-                })
-    mock_df.return_value =  read_data
+        'id': ['650703'], 'state': ['EXECUTED'], 'date': ['2023-09-05T11:30:32Z'], 'amount': ['16210'],
+        'currency_name': ['Sol'], 'currency_code': ['PEN'], 'from': ['Счет 58803664561298323391'],
+        'to': ['Счет 39745660563456619397'], 'description': ['Перевод организации']
+    })
+    mock_df.return_value = read_data
 
     expected_result = [
-            {
-                'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210',
-                'currency_name': 'Sol', 'currency_code': 'PEN', 'from': 'Счет 58803664561298323391',
-                'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'
-            }
-        ]
+        {
+            'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210',
+            'currency_name': 'Sol', 'currency_code': 'PEN', 'from': 'Счет 58803664561298323391',
+            'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'
+        }
+    ]
     result = read_excel('test.csv')
     assert result == expected_result
