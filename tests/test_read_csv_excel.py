2fbde94a40a77c1a1aad1886c3391e4373ced7a1
svetlana-kuprina <kupsveta@rambler.ru>
2025-11-08 14:14:32 +0500

    ДЗ 13.2 add search.py and edit main.py

--- tests/test_read_csv_excel.py
+++ tests/test_read_csv_excel.py
@@ -5,21 +5,30 @@ import pandas as pd
 from src.read_csv_excel import read_csv, read_excel
 
 
-@patch("builtins.open", new_callable=mock_open,
-       read_data='id;state;date;amount;currency_name;currency_code;from;to;description\n650703;EXECUTED;'
-                 '2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;'
-                 'Перевод организации')
+@patch(
+    "builtins.open",
+    new_callable=mock_open,
+    read_data="id;state;date;amount;currency_name;currency_code;from;to;description\n650703;EXECUTED;"
+    "2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;"
+    "Перевод организации",
+)
 def test_read_csv(mock_open):
     """Тест функции read_csv. Используется Mock и patch"""
 
     expected_result = [
         {
-            'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210',
-            'currency_name': 'Sol', 'currency_code': 'PEN', 'from': 'Счет 58803664561298323391',
-            'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
         }
     ]
-    result = read_csv('test.csv')
+    result = read_csv("test.csv")
     assert result == expected_result
 
 
@@ -39,19 +48,33 @@ def test_read_excel_error():
 def test_read_excel(mock_df):
     """Тест функции read_excel. Используется Mock и patch"""
 
-    read_data = pd.DataFrame({
-        'id': ['650703'], 'state': ['EXECUTED'], 'date': ['2023-09-05T11:30:32Z'], 'amount': ['16210'],
-        'currency_name': ['Sol'], 'currency_code': ['PEN'], 'from': ['Счет 58803664561298323391'],
-        'to': ['Счет 39745660563456619397'], 'description': ['Перевод организации']
-    })
+    read_data = pd.DataFrame(
+        {
+            "id": ["650703"],
+            "state": ["EXECUTED"],
+            "date": ["2023-09-05T11:30:32Z"],
+            "amount": ["16210"],
+            "currency_name": ["Sol"],
+            "currency_code": ["PEN"],
+            "from": ["Счет 58803664561298323391"],
+            "to": ["Счет 39745660563456619397"],
+            "description": ["Перевод организации"],
+        }
+    )
     mock_df.return_value = read_data
 
     expected_result = [
         {
-            'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210',
-            'currency_name': 'Sol', 'currency_code': 'PEN', 'from': 'Счет 58803664561298323391',
-            'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
         }
     ]
-    result = read_excel('test.csv')
+    result = read_excel("test.csv")
     assert result == expected_result
---
+++ tests/test_search.py
@@ -0,0 +1,40 @@
+import pytest
+
+from src.search import process_bank_search, process_bank_operations
+
+
+@pytest.mark.parametrize(
+    "search, exp",
+    [
+        (
+            "Перевод организации",
+            [
+                {
+                    "date": "2018-06-30T02:08:58.425572",
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "id": 939719570,
+                    "operationAmount": {"amount": "9824.07", "currency": {"code": "USD", "name": "USD"}},
+                    "state": "EXECUTED",
+                    "to": "Счет 11776614605963066702",
+                },
+                {
+                    "date": "2018-09-12T21:27:25.241689",
+                    "description": "Перевод организации",
+                    "from": "Visa Platinum 1246377376343588",
+                    "id": 594226727,
+                    "operationAmount": {"amount": "67314.70", "currency": {"code": "RUB", "name": "руб."}},
+                    "state": "CANCELED",
+                    "to": "Счет 14211924144426031657",
+                },
+            ],
+        )
+    ],
+)
+def test_process_bank_search(transactions, search, exp):
+    assert process_bank_search(transactions, search) == exp
+
+
+@pytest.mark.parametrize("search, exp", [(["Перевод организации", "Операция"], {"Перевод организации": 2})])
+def test_process_bank_operations(transactions, search, exp):
+    assert process_bank_operations(transactions, search) == exp
